7ce2ae34e72139620f185c3537cba37c5f8c5291
DilrubaJi <91086050+DilrubaJi@users.noreply.github.com>
2021-10-27 20:50:50 +0100

    red failed test for next roverNavigator function

--- MarsRover.test.js
+++ MarsRover.test.js
@@ -21,23 +21,31 @@ describe("current orientation start N, move left 4 times", () => {
     });
 });
 
+
 describe("put directions in an array", () => {
     test("test for each direction letter in an array element", () => {
-        expect(stringToArray("LMLM")).toEqual(["L","M","L","M"]);
+        expect(stringToArray("LMLM")).toEqual(["L", "M", "L", "M"]);
     });
 });
 
 describe("moveRoverOnePlace function compound results", () => {
     test("test N movement increases x and y on top of previous moves", () => {
         expect(moveRoverOnePlace("N")).toBe("0 1");
-        });
+    });
     test("test E movement increases x and y on top of previous moves", () => {
         expect(moveRoverOnePlace("E")).toBe("1 1");
-        });    
+    });
     test("test S movement increases x and y on top of previous moves", () => {
         expect(moveRoverOnePlace("S")).toBe("1 0");
-        });    
+    });
     test("test W movement increases x and y on top of previous moves", () => {
         expect(moveRoverOnePlace("W")).toBe("0 0");
-        });    
+    });
+});
+
+
+describe("roverNavigator takes navigation instructions and returns end coordinates and position direction", () => {
+    test("test single instruction, turn right from North starting", () => {
+        expect(roverNavigator("R")).toBe("E");
+    });
 });
